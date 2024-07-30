9e35cb3e3856aa8009a387af7a7846467d49e248
ayan <ayan.baig@skillassure.com>
2024-07-31 00:53:03 +0530

    4th code

--- mysite/settings_rail.py
+++ mysite/settings_rail.py
@@ -4,4 +4,4 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 
-ALLOWED_HOSTS = ['web-production-f709.up.railway.app']
+ALLOWED_HOSTS = ['web-production-f709.up.railway.app/shot']
