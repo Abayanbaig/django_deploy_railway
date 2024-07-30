628e426c385ec43b19f5458b2f8643e12ac66d26
ayan <ayan.baig@skillassure.com>
2024-07-31 01:07:41 +0530

    code

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("index shop")
+    return render(request , 'shop/index.html')
